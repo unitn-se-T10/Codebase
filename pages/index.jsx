69bc36882fb7ca95635bef845b53576bc7a59f24
Sartor02 <alessandrosartore02@gmail.com>
2022-12-24 19:36:24 +0100

    Done: Homepage; Todo: Pagina ristorante

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,75 @@
 import Layout from "components/layout";
+import React, { useState } from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import {
+  VStack,
+  Box,
+  Text,
+  Input,
+  InputGroup,
+  InputRightElement,
+  useColorModeValue,
+  Center,
+  InputLeftElement,
+  Flex,
+} from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
+
+const RenderHome = () => {
+  return (
+    <Flex
+      justify="center"
+      px={{ base: "0", sm: "8" }}
+      py={{ base: "16", md: "32" }}
+    >
+      <Box w={550} h={500} p={5} borderRadius={20} shadow="dark-lg">
+        <VStack alignItems="center">
+          <Text
+            align="center"
+            mt="20"
+            p="6"
+            textColor="white"
+            fontSize={50}
+            fontWeight="bold"
+          >
+            HUNGRY EVERYWHERE
+          </Text>
+          <InputGroup size="lg">
+            <Input
+              pr="4.5rem"
+              borderRadius={20}
+              placeholder="Indrizzo di consegna"
+              type="type"
+            />
+            <InputRightElement w="9rem" p={1}>
+              <Button h="1.75rem" borderRadius={20} size="sm">
+                Trova Ristoranti
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </VStack>
+      </Box>
+    </Flex>
+  );
+};
 
 export default function Home() {
   return (
-    <Layout>
-      <h1>Hello World</h1>
-    </Layout>
+    <Box
+      bgImage="/sfondo1.png"
+      bgSize="cover"
+      bgPosition="center"
+      bgRepeat="no-repeat"
+    >
+      <Layout>
+        <RenderHome />
+      </Layout>
+    </Box>
   );
 }
---
+++ pages/ristorante/[id].jsx
@@ -0,0 +1,174 @@
+import Layout from "components/layout";
+import React, { useState } from "react";
+import { Grid, GridItem, StackDivider } from "@chakra-ui/react";
+import {
+  VStack,
+  Box,
+  HStack,
+  Text,
+  Divider,
+  Flex,
+  Spacer,
+} from "@chakra-ui/react";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { GiEggClutch, GiWheat } from "react-icons/gi";
+import { StarIcon, TriangleDownIcon } from "@chakra-ui/icons";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+
+const RistorantePage = ({ risto, menu, dish }) => {
+  return (
+    <VStack alignItems="center" p={4} spacing={4}>
+      <HStack spacing={5}>
+        <Box
+          w={200}
+          h={200}
+          bgImage={risto.image}
+          bgSize="cover"
+          bgPosition="center"
+          bgRepeat="no-repeat"
+          borderRadius={20}
+        />
+        <VStack alignItems="start">
+          <Text fontSize={20} fontWeight="bold">
+            {risto.fullName}
+          </Text>
+          <HStack spacing={2}>
+            {[...Array(5).keys()].map((i) =>
+              i < risto.valutazione ? (
+                <StarIcon boxSize={6} key={i} color="yellow.500" />
+              ) : (
+                <StarIcon boxSize={6} key={i} color="gainsboro" />
+              )
+            )}
+          </HStack>
+          <Text>Indirizzo: {risto.address}</Text>
+          <HStack>
+            <Text>
+              Orari:
+              {
+                // TODO: Mettere aperto o chiuso in base al current time e alle informazioni reperite tramite API
+              }
+            </Text>
+            <Text>Chiude alle ore:</Text>
+            <Text>
+              {
+                // TODO: Mettere gli orari prese dall'API
+              }
+            </Text>
+            <TriangleDownIcon />
+          </HStack>
+          <Text>Telefono: {risto.phoneNumber}</Text>
+        </VStack>
+      </HStack>
+      <Divider borderColor="black" />
+      <Tabs
+        align="center"
+        colorScheme="orange"
+        size="md"
+        variant="soft-rounded"
+      >
+        <TabList>
+          <Tab>Antipasti</Tab>
+          <Tab>Primi piatti</Tab>
+          <Tab>Secondi piatti</Tab>
+          <Tab>Contorni</Tab>
+          <Tab>Dessert</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Text>Da riempire</Text>
+          </TabPanel>
+          <TabPanel>
+            <VStack spacing={10}>
+              <HStack spacing={10}>
+                <DishCard dish={dish} />
+                <DishCard dish={dish} />
+              </HStack>
+              <HStack spacing={10}>
+                <DishCard dish={dish} />
+                <DishCard dish={dish} />
+              </HStack>
+              <HStack spacing={10}>
+                <DishCard dish={dish} />
+                <DishCard dish={dish} />
+              </HStack>
+            </VStack>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </VStack>
+  );
+};
+
+const DishCard = ({ dish }) => {
+  return (
+    <Box w={400} p={5} borderRadius={50} bgColor="yellow.200">
+      <Flex>
+        <Box
+          w={75}
+          h={75}
+          bgImage={dish.image}
+          bgSize="cover"
+          bgPosition="center"
+          bgRepeat="no-repeat"
+          borderRadius={50}
+        ></Box>
+        <Spacer />
+        <VStack>
+          <Text>{dish.nome}</Text>
+          <Divider borderColor="black" />
+          <HStack>
+            {/* FIXME: Mettere tutti gli allergeni della pietanza*/}
+            <VStack>
+              <GiEggClutch size="1.75em" />
+              <Text fontSize={10}>Uova</Text>
+            </VStack>
+
+            <VStack>
+              <GiWheat size="1.75em" />
+              <Text fontSize={10}>Grano</Text>
+            </VStack>
+          </HStack>
+        </VStack>
+        <Spacer />
+        <Box w={70} h={70} fontSize={20} borderRadius={50} bgColor="green.300">
+          <Flex align="center" direction="column" h="70%">
+            <Spacer />
+            {dish.prezzo} €
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default function Ristorante() {
+  // TODO: Da fetchare dalle APIs
+  const piatto = {
+    id: "1",
+    nome: "Pasta alla carbonara",
+    prezzo: 6,
+    image: "/carbonara.jpg",
+  };
+
+  // TODO: Da fetchare dalle APIs
+  const ristorante = {
+    id: 1,
+    image: "/image1.jpg",
+    fullName: "Ristorante da Luca Tecchio",
+    address: "Via Martiri della Libertà, 80",
+    phoneNumber: "338-982-3491",
+    email: "ristorante.da.Luca@aziendaImportante.it",
+    valutazione: 4,
+  };
+
+  // TODO: Da fetchare dalle APIs
+  const menu = {};
+
+  return (
+    <Layout>
+      <RistorantePage risto={ristorante} menu={menu} dish={piatto} />
+    </Layout>
+  );
+}
